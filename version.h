90d87ab1907db6396a52ecc08b3cac3e7fd84909
wewan <we.wanna.diem@gmail.com>
2022-05-16 18:58:21 +0200

    Aggiunto codice versione programma

---
+++ version.h
@@ -0,0 +1,13 @@
+//
+// Created by wewan on 16/05/2022.
+//
+
+#ifndef GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+#define GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+
+
+#include <string>
+
+static const std::string VERSION = "First";
+
+#endif //GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
